fded149c21d5e7a442bada869494881c8212ffad
Fabian Taranto <fataranto@gmail.com>
2023-12-06 18:59:50 +0100

    mesh rotations

--- src/script.js
+++ src/script.js
@@ -42,6 +42,7 @@ const material = new THREE.MeshToonMaterial({
     gradientMap: gradientTexture
 })
 
+const objectsDistance = 4
 
 const mesh1 = new THREE.Mesh(
     new THREE.TorusGeometry(1, 0.4, 16, 60),
@@ -55,7 +56,14 @@ const mesh3 = new THREE.Mesh(
     new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
     material)
 
+mesh1.position.y = - objectsDistance * 0
+mesh2.position.y = - objectsDistance * 1
+mesh3.position.y = - objectsDistance * 2
+
+
 scene.add(mesh1, mesh2, mesh3)
+
+const sectionMeshes = [mesh1, mesh2, mesh3]
 
 /**
 * Lights
@@ -115,6 +123,12 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Animate Meshes
+    for (const mesh of sectionMeshes) {
+        mesh.rotation.x = elapsedTime * 0.1
+        mesh.rotation.y = elapsedTime * 0.12
+    }
 
     // Render
     renderer.render(scene, camera)
